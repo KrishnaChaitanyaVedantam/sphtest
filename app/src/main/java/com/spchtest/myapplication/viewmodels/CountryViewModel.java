07b709d9df0c353d238b665096a4f588473ecce3
Krishna Vedantam <vedantamskc@gmail.com>
2019-02-27 08:11:44 +0530

    stored data in shared prefences for cache

--- app/src/main/java/com/spchtest/myapplication/viewmodels/CountryViewModel.java
+++ app/src/main/java/com/spchtest/myapplication/viewmodels/CountryViewModel.java
@@ -1,8 +1,14 @@
 package com.spchtest.myapplication.viewmodels;
 
 import android.app.Application;
+import android.app.ProgressDialog;
+import android.content.SharedPreferences;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.spchtest.myapplication.MainActivity;
+import com.spchtest.myapplication.R;
 import com.spchtest.myapplication.models.CallData;
 import com.spchtest.myapplication.models.Record;
 import com.spchtest.myapplication.retrofitapi.countryApi;
@@ -21,10 +27,14 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import static android.content.Context.MODE_PRIVATE;
+
 public class CountryViewModel extends AndroidViewModel {
 
     private MutableLiveData<CallData> callDataList;
+    ProgressDialog pd;
 
+    SharedPreferences mPrefs;
     public LiveData<CallData> getCallDataList(){
         if(callDataList == null){
             callDataList = new MutableLiveData<CallData>();
@@ -46,12 +56,24 @@ public class CountryViewModel extends AndroidViewModel {
             @Override
             public void onResponse(Call<CallData> call, Response<CallData> response) {
                 CallData res = response.body();
+                SharedPreferences.Editor prefsEditor = mPrefs.edit();
+                Gson gson = new Gson();
+                String json = gson.toJson(res);
+                prefsEditor.putString("CData", json);
+                prefsEditor.commit();
                 callDataList.setValue(response.body());
             }
 
             @Override
             public void onFailure(Call<CallData> call, Throwable t) {
                 Log.d("Error",t.getMessage().toString());
+                Gson gson = new Gson();
+                if(mPrefs.contains("CData")){
+                    Toast.makeText(getApplication(), R.string.no_internet,Toast.LENGTH_LONG).show();
+                    String json = mPrefs.getString("CData", "");
+                    CallData obj = gson.fromJson(json, CallData.class);
+                    callDataList.setValue(obj);
+                }
             }
         });
 
@@ -61,5 +83,6 @@ public class CountryViewModel extends AndroidViewModel {
 
     public CountryViewModel(@NonNull Application application) {
         super(application);
+        mPrefs = application.getSharedPreferences("sptechcalldata",MODE_PRIVATE);
     }
 }
